4256639dd5ffb5c427fc9cb61f97a6e6bd5bed14
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-14 22:01:45 +0545

    .

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -6,20 +6,23 @@ import { useNavigate } from "react-router-dom";
 function Trending() {
   const navigate = useNavigate();
   return (
-    <div className="w-screen h-screen ">
-      <div className="w-full flex  flex-col gap-4">
-        <h1 className="text-2xl p-4 text-center font-semibold w-[20%]  text-zinc-400 mb-4">
-          <i
-            onClick={() => navigate(-1)}
-            className="ri-arrow-left-line text-3xl text-[#6556CD]"
-          ></i>
-          Trending
-        </h1>
-       <div className="flex p-4"> <TopNavbar />
-        <div className=" flex  w-[30%] gap-2">
+    <div className="w-screen h-screen p-6 ">
+      <div className="flex w-full items-center justify-between h-[8vh] ">
+        <div className="flex w-[10%] \">
+          <h1 className="text-2xl font-semibold  text-zinc-400 mb-4">
+            <i
+              onClick={() => navigate(-1)}
+              className="ri-arrow-left-line text-2xl text-[#6556CD]"
+            ></i>
+            Trending
+          </h1>
+        </div>
+        <div className="w-[65%]"> <TopNavbar /></div>
+       
+        <div className=" flex w-[25%] gap-2 ">
           <Dropdown title="Category" options={["tv", "Movie", "all"]} />
           <Dropdown title="Duration" options={["day", "week"]} />
-        </div></div>
+        </div>
       </div>
     </div>
   );
