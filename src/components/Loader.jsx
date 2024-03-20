ca4b02a142448977e49203cdf710b1db552ed7fe
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-20 19:38:01 +0545

    .

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import loader from '/loader.gif'
 function Loader() {
   return (
     <div className='w-screen h-screen flex items-center justify-center bg-[#010220]'>
-        <img className='object-cover h-[100%]' src={loader} alt="" />
+        <img className='object-cover h-[50%]' src={loader} alt="" />
     </div>
   )
 }
